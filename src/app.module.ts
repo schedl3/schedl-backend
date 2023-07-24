f6b79344d9eee9b8f078592e8b6bd8f37a9ef368
tomo <tomo@crowdeo.com>
2023-07-24 10:38:04 +0700

    fix: express-sessions via nestjs-session for siwe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { NestSessionOptions, SessionModule } from 'nestjs-session';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatsModule } from './cats/cats.module';
 import { AuthModule } from './auth/auth.module';
@@ -7,6 +8,9 @@ import { AppController } from './app.controller';
 
 @Module({
   imports: [
+    SessionModule.forRoot({
+      session: { secret: 'keyboard cat' },
+    }),
     // MongooseModule.forRoot('mongodb://localhost:27017/test'),
     MongooseModule.forRoot('mongodb://localhost:27017/cats'),
     CatsModule,
