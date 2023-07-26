0671ebb138955c7460b3674f1d601cbe93abb695
tomo <tomo@crowdeo.com>
2023-07-26 22:05:37 +0700

    feat: store sessions in mongodb w/ connect-mongodb-session fix: re-add password field in `UserSchema`

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,22 @@ import { CatsModule } from './cats/cats.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
+import * as session from 'express-session';
+import * as connectMongoDbSession from 'connect-mongodb-session';
+
+const MongoDBStore = connectMongoDbSession(session);
 
 @Module({
   imports: [
     SessionModule.forRoot({
-      session: { secret: 'keyboard cat' },
+      session: {
+        secret: 'keyboard cat',
+        store: new MongoDBStore({
+          uri: 'mongodb://localhost:27017/cats',
+          collection: 'sessions'
+        }),
+      },
     }),
-    // MongooseModule.forRoot('mongodb://localhost:27017/test'),
     MongooseModule.forRoot('mongodb://localhost:27017/cats'),
     CatsModule,
     AuthModule,
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -16,6 +16,9 @@ export class User {
 
   @Prop()
   description: string;
+
+  @Prop()
+  password: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
