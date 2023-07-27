247c419104cb97ef60bba5f625f2de3d89c81daf
tomo <tomo@crowdeo.com>
2023-07-28 02:08:12 +0700

    fix: gm.xmtp.eth not resolving

--- src/xmtp/xmtp.service.ts
+++ src/xmtp/xmtp.service.ts
@@ -7,7 +7,8 @@ export class XmtpService {
   async genGmWallet() {
     const wallet = ethers.Wallet.createRandom();
     const xmtp = await Client.create(wallet, { env: "dev" });
-    const conversation = await xmtp.conversations.newConversation('gm.xmtp.eth');
+    const WALLET_TO = "0x20B572bE48527a770479744AeC6fE5644F97678B"; // gm.xmtp.eth not resolving
+    const conversation = await xmtp.conversations.newConversation(WALLET_TO);
     console.log("Conversation created", conversation);
     const message = await conversation.send("gm");
     console.log("Message sent", message);
