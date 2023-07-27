894a7b00d4e4d3050aeeda3884ba7d9a89404ea6
tomo <tomo@crowdeo.com>
2023-07-28 02:27:51 +0700

    fix: import WalletsModule in XmtpModule for ERROR [ExceptionHandler] Nest can't resolve dependencies of the XmtpService (?)

--- src/xmtp/xmtp.module.ts
+++ src/xmtp/xmtp.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { XmtpService } from './xmtp.service';
 import { XmtpController } from './xmtp.controller';
+import { WalletsModule } from '../wallets/wallets.module';
 
 @Module({
+  imports: [WalletsModule],
   providers: [XmtpService],
   controllers: [XmtpController],
 })
