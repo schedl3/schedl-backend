057b7ef8d2dac30dd68d6b534ad290a7f4098618
tomo <tomo@crowdeo.com>
2023-07-28 10:32:32 +0700

    feat: split genGmWallet into genWallet, gm

--- src/xmtp/xmtp.service.ts
+++ src/xmtp/xmtp.service.ts
@@ -7,25 +7,26 @@ import { WalletsService } from '../wallets/wallets.service';
 export class XmtpService {
   constructor(private readonly walletsService: WalletsService) {}
 
-  async genGmWallet() {
+  async genWallet() {
     const wallet = ethers.Wallet.createRandom();
-    // let xmtpOpts = { env: "dev" };
-    let xmtpOpts = { env: "production" };
-    const xmtp = await Client.create(wallet, { env: "production" });
-    // const WALLET_TO = "0x20B572bE48527a770479744AeC6fE5644F97678B"; // gm.xmtp.eth not resolving
-    const WALLET_TO = '0x78a74b5D1A86704c573163C3aafB6e7234c9Da1e';
-    const conversation = await xmtp.conversations.newConversation(WALLET_TO);
-    console.log("Conversation created", conversation);
-    const message = await conversation.send("gm");
-    console.log("Message sent", message);
-
-    // Save the wallet using the WalletsService
     await this.walletsService.create({
       privateKey: wallet.privateKey,
       publicKey: wallet.publicKey,
       address: wallet.address,
     });
+    return wallet;
+  }
 
+  async gm(wallet) {
+    let xmtpOpts = { env: "production" };
+    const xmtp = await Client.create(wallet, { env: "production" });
+    const WALLET_TO = '0x78a74b5D1A86704c573163C3aafB6e7234c9Da1e';
+    const conversation = await xmtp.conversations.newConversation(WALLET_TO);
+    console.log("Conversation created", conversation);
+    const message = await conversation.send("gm");
+    console.log("Message sent", message);
+  }
+}
     // will block
     // for await (const message of await xmtp.conversations.streamAllMessages()) {
     //   console.log(`New message from ${message.senderAddress}: ${message.content}`);
