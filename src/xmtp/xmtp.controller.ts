0c73870ea24f204e676e52c64c4e7234fddf7c3e
tomo <tomo@crowdeo.com>
2023-07-28 12:16:53 +0700

    feat: split genGmWallet route into genWallet, gm

--- src/xmtp/xmtp.controller.ts
+++ src/xmtp/xmtp.controller.ts
@@ -7,8 +7,14 @@ export class XmtpController {
   constructor(private readonly xmtpService: XmtpService) {}
 
   // @UseGuards(AuthGuard('local'))
-  @Post('genGmWallet')
-  async genGmWallet() {
-    await this.xmtpService.genGmWallet();
+  @Post('genWallet')
+  async genWallet() {
+    return await this.xmtpService.genWallet();
+  }
+
+  // @UseGuards(AuthGuard('local'))
+  @Post('gm')
+  async gm(@Body() body: { wallet: Wallet }) {
+    return await this.xmtpService.gm(body.wallet);
   }
 }
