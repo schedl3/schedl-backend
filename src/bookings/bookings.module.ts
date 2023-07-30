8cf8a886ef0c3b117cf0d90d0479ac9aaf8fb442
tomo <tomo@crowdeo.com>
2023-07-31 00:33:43 +0700

    feat: any user can request meeting if time in schedule

--- src/bookings/bookings.module.ts
+++ src/bookings/bookings.module.ts
@@ -3,12 +3,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { BookingsController } from './bookings.controller';
 import { BookingsService } from './bookings.service';
 import { Booking, BookingSchema } from './schemas/booking.schema';
+import { UsersService } from '../users/users.service';
+import { UsersModule } from '../users/users.module';
+import { User, UserSchema } from '../users/schemas/user.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Booking.name, schema: BookingSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Booking.name, schema: BookingSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    UsersModule,
+  ],
   controllers: [BookingsController],
-  providers: [BookingsService],
+  providers: [BookingsService, UsersService],
   exports: [BookingsService],
 })
-
 export class BookingsModule {}
