f50d0b97761e8b7464ab19dfc0b196e03c1247bb
tomo <tomo@crowdeo.com>
2023-08-01 23:57:10 +0700

    feat: enableCors http://localhost:3001

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { UsersService } from './users/users.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   const usersService = app.get(UsersService);
   // await usersService.createSampleUsers();
   await app.listen(3000);
