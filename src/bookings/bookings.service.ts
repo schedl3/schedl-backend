c201612dd19d6633f22acc8ab863bca7419ae232
tomo <tomo@crowdeo.com>
2023-08-06 17:50:42 +0700

    refactor: schedule time conversion

--- src/bookings/bookings.service.ts
+++ src/bookings/bookings.service.ts
@@ -3,20 +3,60 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateBookingDto } from './dto/create-booking.dto';
-import { Booking, BookingDocument } from './schemas/booking.schema';
+import { Booking } from './schemas/booking.schema';
 import { Schedule } from '../users/schemas/user.schema';
 import { UsersService } from '../users/users.service';
 import { XmtpService } from '../xmtp/xmtp.service';
 
+type ScheduleCSVByDay = Schedule;
+
+type WeekHour = number & { __weekHoursBrand: never };
+type Partial24hTime = string & { __partial24hTimeBrand: never };
+
+const validateWeekHour = (value: number): WeekHour => {
+  if (value < 0 || value > 24*7) {
+    throw new Error('WeekHour must be between 0 and 168.');
+  }
+  return value as WeekHour;
+};
+
+const validatePartial24hTime = (value: string): Partial24hTime => {
+  // Regular expression to match 24-hour time format
+  const regex = /^(?:2[0-3]|[01]?[0-9])(?::([0-5]?[0-9]))?$/;
+
+  if (!regex.test(value)) {
+    throw new Error('Invalid partial 24-hour time format. Use HH or HH:mm format.');
+  }
+
+  return value as Partial24hTime;
+};
+
+const toIsoHHmm = (p24HTime: Partial24hTime): string => {
+  const [HH, mm] = p24HTime.split(':');
+  const isoHHmm = HH.padStart(2, '0') + ':' + (mm === undefined ? '00' : mm);
+  return isoHHmm;
+}
+
+interface WeekHourRange {
+  start: WeekHour;
+  end: WeekHour;
+}
+
+const validateWeekHourRange = ({start, end}) => {
+  validateWeekHour(start);
+  validateWeekHour(end);
+  return { start, end } as WeekHourRange;
+}
+
 // Given a day and a time return the number of hours from the beginning of the week.
-function getOffsetFromBeginningOfWeek(day: string, hhmm: string) {
+function toWeekHour(day: string, isoHHmm: string): WeekHour {
   // XXX luxon week starts Monday
   const dayIdx = { Mon: 0, Tue: 1, Wed: 2, Thu: 3, Fri: 4, Sat: 5, Sun: 6 };
   // t0 is 00:00 today, t1 is the time X days from now
   const t0 = DateTime.fromFormat('00:00', 'HH:mm');
-  const t1 = DateTime.fromFormat(hhmm, 'HH:mm').plus({ days: dayIdx[day] });
-  const offsetInHrs = Interval.fromDateTimes(t0, t1).length('hours');
-  return offsetInHrs;
+  const t1 = DateTime.fromFormat(isoHHmm, 'HH:mm').plus({ days: dayIdx[day] });
+  const wh = validateWeekHour(Interval.fromDateTimes(t0, t1).length('hours'));
+  return wh;
 }
 
 // [
@@ -28,27 +68,22 @@ function getOffsetFromBeginningOfWeek(day: string, hhmm: string) {
 //   { start: 105, end: 113.5 },
 //   { start: 129, end: 131.5 }
 // ]
-function getStartEndTimes(schedule: Schedule) {
+function toWeekHourRanges(schedule: ScheduleCSVByDay) {
   const weekDays = Object.keys(schedule);
-  const startEndTimesPerDay = {};
+  const rangesByDay = {};
 
   weekDays.forEach((day) => {
-    const intervals = schedule[day].split(",");
-    const startEndTimes = intervals.map((interval) => {
-      const [startTime, endTime] = interval.split("-").map((time) => {
-        const [h, m] = time.split(':');
-        const hhmm = h.padStart(2, '0') + ':' + (m === undefined ? '00' : m);
-        const offsetStart = getOffsetFromBeginningOfWeek(day, hhmm);
-        return offsetStart;
+    const csv = schedule[day];
+    rangesByDay[day] = csv.split(",").map((partialRange) => {
+      const [start, end] = partialRange.split("-").map((s) => {
+        const p24HTime = validatePartial24hTime(s);
+        return toWeekHour(day, toIsoHHmm(p24HTime));
       });
-      return { start: startTime, end: endTime };
+      return { start, end } as WeekHourRange;
     });
-
-    startEndTimesPerDay[day] = startEndTimes;
   });
 
-  return [].concat(...Object.values(startEndTimesPerDay));
-  // return startEndTimesPerDay;
+  return [].concat(...Object.values(rangesByDay));
 }
 
 function getOffsetFromUTC(tz) {
@@ -56,23 +91,23 @@ function getOffsetFromUTC(tz) {
   return dt.offset / 60;
 }
 
-// convert from non-utc to utc to all the available hour intervals during a 168 hour week
-function normalizeTzOffset(times, offset) {
+// convert from non-utc to utc to all the available hour ranges during a 168 hour week
+function normalizeTzOffset(ranges: Array<WeekHourRange>, offset) {
   const hoursInWeek = 24 * 7;
-  return times.map(({ start, end }) => ({
+  return ranges.map(({ start, end }) => ({
     start: start - offset,
     end: end - offset,
-  })).flatMap(({ start, end }) => {
+  } as WeekHourRange)).flatMap(({ start, end }: WeekHourRange): Array<WeekHourRange> => {
     if (start < 0 && end < 0) {
-      return [{ start: start + hoursInWeek, end: end + hoursInWeek }];
+      return [validateWeekHourRange({ start: start + hoursInWeek, end: end + hoursInWeek })];
     } else if (start > hoursInWeek && end > hoursInWeek) {
-      return [{ start: start - hoursInWeek, end: end - hoursInWeek }];
+      return [validateWeekHourRange({ start: start - hoursInWeek, end: end - hoursInWeek })];
     } else if (start < 0) {
-      return [[{ start: start + hoursInWeek, end: hoursInWeek }], [{ start: 0, end: end }]];
+      return [validateWeekHourRange({ start: start + hoursInWeek, end: hoursInWeek }), validateWeekHourRange({ start: 0, end: end })];
     } else if (end > hoursInWeek) {
-      return [[{ start: start, end: hoursInWeek }], [{ start: 0, end: end - hoursInWeek }]];
+      return [validateWeekHourRange({ start: start, end: hoursInWeek }), validateWeekHourRange({ start: 0, end: end - hoursInWeek })];
     }
-    return [[{ start, end }]];
+    return [validateWeekHourRange({ start, end })];
   });
 }
 
@@ -130,13 +165,12 @@ export class BookingsService {
     if (meetingDateTime.minute % lengthMinutes !== 0) {
       throw new Error("Invalid meeting start time.");
     }
+    let times = toWeekHourRanges(toUser.schedule);
+    console.log('orig:', times);
     const tz = toUser.tz ? toUser.tz : 'utc';
     const off = getOffsetFromUTC(tz);
-    let times = getStartEndTimes(toUser.schedule);
-    console.log(times);
-
     times = normalizeTzOffset(times, off);
-    console.log(times);
+    console.log('norm:', times);
     // Calculate the start time and end time of the meeting in the week
     const beginningOfWeek = meetingDateTime.startOf('week');
     const startMeet = meetingDateTime.diff(beginningOfWeek, 'minutes').minutes / 60;
